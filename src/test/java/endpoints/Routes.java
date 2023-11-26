c4a4e0d09284d7d1230b772a9d77d5135b8b71c3
Simi Francis T <42520712+SimiFrancis@users.noreply.github.com>
2023-11-26 16:48:03 +0400

    Update Routes.java

--- src/test/java/endpoints/Routes.java
+++ src/test/java/endpoints/Routes.java
@@ -3,5 +3,5 @@ package endpoints;
 public class Routes {
     public static String base_url="https://www.googleapis.com/geolocation";
     public static String version = "/v1";
-    public static String post_url=base_url+version+"/geolocate?key=AIzaSyBlzPbU5AtO6SJ_35Sbc8FnAfsTi5a-mQQ";
+    public static String post_url=base_url+version+"/geolocate?key=key";
 }
